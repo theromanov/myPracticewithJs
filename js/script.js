7504e79262b0ca0f4e1e074909bf81b969eb672e
Roman <romansyplyviy@gmail.com>
2026-04-08 20:35:28 +0100

    feat: Translate code to English, add input validation and viewer feedback

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 "use strict";
 
-const numberOfFilms = +prompt("Скільки фільмів ви вже переглянули?", "");
+const numberOfFilms = +prompt("How many movies have you watched already?", "");
 
 const personalMovieDB = {
   count: numberOfFilms,
@@ -10,11 +10,29 @@ const personalMovieDB = {
   privat: false,
 };
 
-for (let i = 0; i < 2; i++) {
-  let a = prompt("Один з останніх переглянутих фільмів?", ""),
-    b = +prompt("На скільки оціните його?", "");
+let a, b;
 
-  personalMovieDB.movies[a] = b;
+for (let i = 0; i < 2; i++) {
+  a = prompt("One of the last movies you watched?", "");
+  b = +prompt("How would you rate it?", "");
+
+  if (a != "" && b != "" && a != null && b != null && a.length < 50) {
+    personalMovieDB.movies[a] = b;
+    console.log("Data successfully recorded!");
+  } else {
+    console.log("Error. Please try again!");
+    i--;
+  }
+}
+
+if (personalMovieDB.count < 10) {
+  console.log("You've watched quite few movies!");
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count <= 30) {
+  console.log("You are a classic viewer!");
+} else if (personalMovieDB.count > 30) {
+  console.log("You are a movie fan!");
+} else {
+  console.log("An error occurred!");
 }
 
 console.log(personalMovieDB);
